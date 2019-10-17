cc1936780e206cd346f7dc90e2f5324dc87962e4
stephanebressani <s.bressani@bluewin.ch>
2019-10-17 10:08:10 +0200

    try

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from astropyfr import astropyfr
 
 def index(request):
     return render(request, 'index.html', {})
 
 def hello(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    astro = astropyfr.astropyfr('2000/04/04', '12:24', '+02:00', '46n12', '6e9')
+    return HttpResponse("Hello, world. You're at the polls index." + astro.get_data())
