165ea74b429625c6f4e3d671c71c8ff9c20895c7
stephanebressani <s.bressani@bluewin.ch>
2019-10-19 17:18:18 +0200

    for debug with real data

--- home/views.py
+++ home/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request, 'index.html', {})
 
 def hello(request):
-    astro = astropyfr.astropyfr('2000/04/04', '12:24', '+02:00', '46n12', '6e9')
+    astro = astropyfr.astropyfr('1986/04/03', '04:54', '+02:00', '46n12', '6e9')
     return HttpResponse(astro.get_data())
