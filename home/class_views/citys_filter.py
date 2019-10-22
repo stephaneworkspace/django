c4c804e54cb8eea10661fbec5eb3bfdc4ad5cfab
stephanebressani <s.bressani@bluewin.ch>
2019-10-23 00:58:56 +0200

    filter work in progress

--- home/class_views/citys_filter.py
+++ home/class_views/citys_filter.py
@@ -1,14 +1,13 @@
-from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from django.http import HttpResponse, JsonResponse
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status , generics
 from rest_framework.filters import BaseFilterBackend
 from rest_framework_swagger import renderers
+from ..settings import BASE_DIR
 import coreapi
 import coreschema
 import os
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 import json
 with open(os.path.join(BASE_DIR, 'assets/citys.json')) as f:
     data = json.load(f)
@@ -49,6 +48,70 @@ class citys_filter(generics.GenericAPIView):
         f_dict = { k:v for d in f for k,v in d.items() }
         
         # print(data)
+        #return JsonResponse([x for x in data if x['name']])
         
+        """
+        a = []
+        sw = False
+        for d in data:
+            sw = False
+            for k, v in d.items():
+                if (k == 'name'):
+                    sw = filter(filter_name, v)
+            if (sw):
+                a.append(d)
+        return HttpResponse(a)
+        
+        
+        """
+        filter_arr = []
+        filter_arr = filter_name(name_json(True), [f_dict['name'].lower()])
+        print(filter_arr)
+        
+        arr = []
+        sw = False 
+        for d in data:
+            sw = False
+            for k, v in d.items():
+                if k == 'name':
+                    if v.lower() in filter_arr:
+                        sw = True
+            if sw:
+                arr.append(d)
+        return HttpResponse(arr)
+
         # astro = astropyfr.astropyfr(f_dict["year_month_day"], f_dict["hour_min"], utc, lat, lng)
         return HttpResponse('ok + ' + f_dict['name'])
+
+def name_json(sw_lower):
+    a = [] 
+    for d in data:
+        for k, v in d.items():
+            if k == 'name':
+                if sw_lower:
+                    a.append(v.lower())
+                else:
+                    a.append(v)
+    return a
+
+string_total = ['tes', 'test', 't', 'test1234', '1234test']
+def filter_name(string, substr):
+    
+    # return [s for s in string if any(sub in s for sub in substr)]
+
+    return [s for s in string if any(sub in s for sub in substr)]
+ 
+    #return[s for s in string 
+    #if re.match(r'[^\d]+|^', s).group(0) in substr]
+"""
+def filter_name(name):
+    a = [] 
+    for d in data:
+        for k, v in d.items():
+            if (k == 'name'):
+                a.append(v)
+    if (name in a):
+        return True
+    else:
+        return False
+"""
